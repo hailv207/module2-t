e4699347a1fa33057fda6c6475765514853b9b90
vuhaile <levuhai207@hotmail.com.vn>
2020-06-18 16:54:41 +0700

    update bai thuc hanh TDD tinh ngay tiep theo

--- week2/src/test/java/NextDayCalculatorTest.java
+++ week2/src/test/java/NextDayCalculatorTest.java
@@ -9,46 +9,57 @@ import static org.junit.jupiter.api.Assertions.*;
 public class NextDayCalculatorTest {
 
     @Test
-    @DisplayName("case 28/2/2020")
-    public void testNextday2020Feb28() {
-        int day = 28;
-        int month = 2;
-        int year = 2020;
+    @DisplayName("case 1/1/2018")
+    public void testNextday2018Jan1() {
+        int day = 1;
+        int month = 1;
+        int year = 2018;
 
-        int[] expectedResult = {29, 2, 2020};
+        int[] expectedResult = {2, 1, 2018};
         assertArrayEquals(expectedResult, NextDayCalculator.getNextDay(day, month, year));
     }
 
     @Test
-    @DisplayName("case 31/12/2020")
-    public void testNextday2020Dec31() {
+    @DisplayName("case 31/1/2018")
+    public void testNextday2018Jan31() {
         int day = 31;
-        int month = 12;
-        int year = 2020;
+        int month = 1;
+        int year = 2018;
 
-        int[] expectedResult = {1, 1, 2021};
+        int[] expectedResult = {1,2 , 2018};
         assertArrayEquals(expectedResult, NextDayCalculator.getNextDay(day, month, year));
     }
 
     @Test
-    @DisplayName("case 28/2/2021")
-    public void testNextday2021Feb28() {
+    @DisplayName("case 30/4/2018")
+    public void testNextday2018Apr30() {
+        int day = 30;
+        int month = 4;
+        int year = 2018;
+
+        int[] expectedResult = {1, 5, 2018};
+        assertArrayEquals(expectedResult, NextDayCalculator.getNextDay(day, month, year));
+    }
+
+    @Test
+    @DisplayName("case 28/2/2018")
+    public void testNextday2018Feb28() {
         int day = 28;
         int month = 2;
-        int year = 2021;
+        int year = 2018;
 
-        int[] expectedResult = {1, 3, 2021};
+        int[] expectedResult = {1, 3, 2018};
         assertArrayEquals(expectedResult, NextDayCalculator.getNextDay(day, month, year));
     }
 
     @Test
-    @DisplayName("case 30/8/2020")
-    public void testNextday2020Aug30() {
-        int day = 30;
-        int month = 8;
+    @DisplayName("case 29/2/2020")
+    public void testNextday2020Feb29() {
+        int day = 29;
+        int month = 2;
         int year = 2020;
 
-        int[] expectedResult = {31, 8, 2020};
+        int[] expectedResult = {1, 3, 2020};
         assertArrayEquals(expectedResult, NextDayCalculator.getNextDay(day, month, year));
     }
 
