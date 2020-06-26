7d380f3400b5bbe14d2a3485a1da469250d346d6
vuhaile <levuhai207@hotmail.com.vn>
2020-06-26 13:57:20 +0700

    update

--- week3/src/main/java/BinarySearch.java
+++ week3/src/main/java/BinarySearch.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BinarySearch {
     public static void main(String[] args) {
         int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
@@ -7,6 +9,17 @@ public class BinarySearch {
         System.out.println(binarySearch(list, 1));  /*-1 */
         System.out.println(binarySearch(list, 5));  /*-1 */
         System.out.println(binarySearch(list, 80)); /*-1 */
+
+        System.out.println("Array:");
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        int key = scanner.nextInt();
+        System.out.println("Binary search result: " + binarySearch(list, key));
+        System.out.println("Binary search using recursion result: " + recursionBinarySearch(list, key, 0, list.length - 1));
+
     }
 
     static int binarySearch(int[] list, int key) {
@@ -22,5 +35,19 @@ public class BinarySearch {
                 low = mid + 1;
         }
         return -1; /* Now high < low, key not found */
+    }
+
+    static int recursionBinarySearch(int[] list, int key, int low, int high) {
+        int mid = (high + low) / 2;
+        if (list[mid] == key) {
+            return mid;
+        }
+        if (key < list[mid]) {
+            return recursionBinarySearch(list, key, low, mid - 1);
+        }
+        if (key > list[mid]) {
+            return recursionBinarySearch(list, key, mid + 1, high);
+        }
+        return -1;
     }
 }
